b38254aac4087af258f51b469b2cb82db03afa54
Lionel Chew <92838415+LC-GHub@users.noreply.github.com>
2024-11-13 20:55:08 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,18 @@ import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 
-
 dotenv.config();
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(), {
     cors: true,
-    logger: ['error', 'warn', 'log', 'debug', 'verbose'] // <--- Add this line in options object
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'], // <--- Add this line in options object
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
+
+// EMAIL_USER=workylappy05@gmail.com
+// EMAIL_PASS=rkus erct ktpl vnvu
